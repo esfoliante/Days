15255b2650a7a220b0b9451dd84807ad7e813921
Miguel Ferreira <miguel.ferreira.2003@outlook.com>
2021-03-13 16:25:30 +0000

    api <feat>: created sessions migration

---
+++ days_api/database/migrations/2021_03_13_162046_create_sessions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id')->unsigned()->nullable();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('session_id');
+            $table->string('device_information');
+            $table->string('ip');
+            $table->string('browser');
+            $table->timestamp('last_login_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
