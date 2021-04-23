2267f93162df72cbf88f8a3693ad49ef13492e54
Miguel Ferreira <miguel.ferreira.2003@outlook.com>
2021-04-23 12:53:11 +0100

    backoffice <fix>: backoffice as module

---
+++ days_backoffice/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <img src="/logo.svg" alt="Icon" class="AppLogo" />
+</template>
+
+<style>
+.AppLogo {
+  animation: 1s appear;
+  margin: auto;
+  width: 30vw;
+}
+
+@media only screen and (max-width: 600px) {
+  .AppLogo {
+    width: 70vw;
+  }
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
