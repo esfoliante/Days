45cb76e7f5fc6bed4fdb492f9ae3fd0ca4b8e8cd
Miguel Ferreira <30320326@colgaia.pt>
2021-06-25 00:55:43 +0100

    api <feat>: schedule working with api

--- days/app/Http/Resources/ScheduleResource.php
+++ days/app/Http/Resources/ScheduleResource.php
@@ -13,7 +13,10 @@ class ScheduleResource extends JsonResource
     public function toArray($request)
     {
         return [
-
+            'subject' => $this->subject->name,
+            'starts_at' => $this->starts_at,
+            'ends_at' => $this->ends_at,
+            'day_week' => $this->day_week,
         ];
     }
 }
